48f9e25fc74d00806403e583bff3967ef3885e9e
Mary Mosman <mary@techelevator.com>
2022-01-24 18:57:48 -0600

    lecture starter code & recording updates

---
+++ module-1/07_Collections_Part_1_Lists_Stacks_Queues/lecture/src/main/java/com/techelevator/Lecture.java
@@ -0,0 +1,13 @@
+package com.techelevator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lecture {
+
+	public static void main(String[] args) {
+
+
+	}
+}
