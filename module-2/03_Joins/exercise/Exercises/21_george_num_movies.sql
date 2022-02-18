3ed85b399774168391690284d3b4eb7c2278a9bc
Ryan Hunter <hunterryan0000@yahoo.com>
2022-02-18 08:53:49 -0600

    #21 i own you

--- module-2/03_Joins/exercise/Exercises/21_george_num_movies.sql
+++ module-2/03_Joins/exercise/Exercises/21_george_num_movies.sql
@@ -1,8 +1,8 @@
 -- 21. For every person in the person table with the first name of "George", list the number of movies they've been in--include all Georges, even those that have not appeared in any movies. Display the names in alphabetical order. (59 rows)
 -- Name the count column 'num_of_movies'
-select person_name, count(*) as num_of_movies from person
+select person_name, count(movie) as num_of_movies from person
 left join movie_actor on person.person_id = movie_actor.actor_id
 left join movie on movie_actor.movie_id = movie.movie_id
 where person_name Like 'George %'
-group by person_name
+group by person_id, person_name
 order by person_name;
