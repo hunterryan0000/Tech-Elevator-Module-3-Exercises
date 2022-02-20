9faddf2c800d7978cd523f6fab5528f842190e0d
Ryan Hunter <hunterryan0000@yahoo.com>
2022-02-19 19:01:47 -0600

    Using IN instead of = was a tricky one

--- module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/exercise/Exercises/11_add_director.sql
@@ -1,2 +1,6 @@
--- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet. Add yourself to the person table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
-
+-- 11. Hollywood is remaking the classic movie "The Blob" and doesn't have a director yet.
+--Add yourself to the person table, and assign yourself as the director of "The Blob" (the movie is already in the movie table). (1 record each)
+insert into person (person_name)
+values ('Ryan Hunter');
+update movie set director_id = (select person_id from person where person_name = 'Ryan Hunter')
+where movie_id = '367220';
